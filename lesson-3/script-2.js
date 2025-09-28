77aedf24ad1e65e3ec2e064c0369c4d81dd42491
Vladyslav Soloviov <soloviov1654@gmail.com>
2025-09-28 13:46:49 +0300

    homework 3-2 done

---
+++ lesson-3/script-2.js
@@ -0,0 +1,8 @@
+const num1 = prompt("Enter first value");
+const num2 = prompt("Enter second value");
+const num3 = prompt("Enter third value");
+console.log(`
+    First value: ${num1}
+    Second value: ${num2}
+    Third value: ${num3}
+`);
