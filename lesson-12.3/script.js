1749b61f0505ae452252dd22433979e167243044
Vladyslav Soloviov <soloviov1654@gmail.com>
2025-10-30 19:43:09 +0200

    homework 12.3 fix

--- lesson-12.3/script.js
+++ lesson-12.3/script.js
@@ -1,22 +1,21 @@
 const parent = document.querySelector("ul");
-const addBtn = document.querySelector("#add");
+const form = document.querySelector("form");
 const input = document.querySelector("input");
 
-addBtn.addEventListener("click", () => {
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
   const li = document.createElement("li");
   li.className = "wrapper";
   const deleteBtn = document.createElement("button");
   deleteBtn.textContent = "delete";
-  li.textContent = input.value;
+  li.textContent = input.value + " ";
   li.appendChild(deleteBtn);
   parent.appendChild(li);
   input.value = "";
-  
-  deleteBtn.addEventListener("click", () => {
-    li.parentNode.removeChild(li);
-  });
 });
 
-document.querySelector("form").addEventListener("submit", (event) => {
-  event.preventDefault();
+parent.addEventListener("click", (event) => {
+  if (event.target.tagName === "BUTTON") {
+    event.target.parentElement.remove();
+  }
 });
