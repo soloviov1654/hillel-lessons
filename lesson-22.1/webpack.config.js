fe0dea0a372cd4bb7c29e1683786a02d955feea9
Vladyslav Soloviov <soloviov1654@gmail.com>
2025-12-10 17:44:02 +0200

    homework 22.1 done

---
+++ lesson-22.1/webpack.config.js
@@ -0,0 +1,39 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(process.cwd(), "dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+    static: "./dist",
+    port: 3000,
+    open: true,
+  },
+  mode: "development",
+};
